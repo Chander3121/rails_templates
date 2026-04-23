45de7d31aa934a094fcb40e1a62f7a98551e7582
Chander Prakash <cpkc3121@gmail.com>
2026-04-23 13:40:23 +0530

    Add expiry time for token

--- app/services/jwt_service.rb
+++ app/services/jwt_service.rb
@@ -3,11 +3,19 @@ class JwtService
   SECRET = Rails.application.credentials.secret_key_base
 
   def self.encode(payload)
+    payload[:exp] = 24.hours.from_now.to_i
+    payload[:iat] = Time.now.to_i
     JWT.encode(payload, SECRET)
   end
 
   def self.decode(token)
-    JWT.decode(token, SECRET)[0]
+    decoded = JWT.decode(token, SECRET)[0]
+
+    if decoded["exp"] && Time.now.to_i > decoded["exp"]
+      return nil
+    end
+
+    decoded
   rescue
     nil
   end
